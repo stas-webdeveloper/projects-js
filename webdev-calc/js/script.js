c8e5a406f29010b7f163dadf4b0af0bb1ff8bbe8
stas-webdeveloper <ivanstepanoff@yahoo.com>
2022-12-25 21:19:21 +0200

    refact: make two global event listeners instead of many individual

--- webdev-calc/js/script.js
+++ webdev-calc/js/script.js
@@ -1,125 +1,147 @@
-// TODO: +refact: swap id's/classes for data attr's in .js
-// TODO: refact: solo document.EventListener() instead of bunch of individual ones
-// TODO: chore: change html tags on semantic ones
-// TODO: +chore: discard id's
-// TODO: tests
-let total = 5000,
-    time = 1,
-    hourRate,
+// TODO: fix: TDD
+const landBasePrice = 1750,
+    multipageBasePrice = 9000,
+    cmsPrice = 4000,
+    editTextPrice = 1000,
+    blockPrice = 350,
+    landBlocksCount = 5,
+    pagePrice = 1500,
+    pagesBaseCount = 6,
+    defaultHours = 1,
+    defaultHourRate = 350,
+
     tabLeft = document.querySelector('[data-tab-left]'),
     tabRight = document.querySelector('[data-tab-right]'),
-    blocksBlock = document.querySelector('[data-blocks-counter]'),
-    pagesBlock = document.querySelector('[data-pages-counter]'),
-    counterBlock = document.querySelector('[data-blocks-input]'),
-    counterPages = document.querySelector('[data-pages-input]'),
-    counterHours = document.querySelector('[data-hours-input]'),
-    counterRate = document.querySelector('[data-rate-input]'),
-    changesCheck = document.querySelector('[data-edit-check]'),
+    blocksCounter = document.querySelector('[data-blocks-counter]'),
+    pagesCounter = document.querySelector('[data-pages-counter]'),
+    blocksInput = document.querySelector('[data-blocks-input]'),
+    pagesInput = document.querySelector('[data-pages-input]'),
+    hoursInput = document.querySelector('[data-hours-input]'),
+    rateInput = document.querySelector('[data-rate-input]'),
+    editCheck = document.querySelector('[data-edit-check]'),
     cmsCheck = document.querySelector('[data-cms-check]'),
-
     totalValue = document.querySelector('[data-total-count]'),
     input = document.querySelectorAll('input');
 
-const land = 5000,
-    corp = 12000,
-    cms = 4000,
-    changes = 1000,
-    blocks = 500,
-    pages = 2500;
+let total,
+    totalChecked = 0,
+    blocksCount,
+    pagesCount,
+    hours = defaultHours,
+    hourRate = defaultHourRate;
+
+onTabLeft();
 
 document.addEventListener('click', e => {
     const targetDataset = e.target.dataset;
-    console.log('targetDataset:', targetDataset)
 
-    if ('tabLeft' in targetDataset) {
-        for (let i = 0; i < input.length; i++) {
-            input[i].value = '';
-        }
+    if ('tabLeft' in targetDataset) onTabLeft()
+    if ('tabRight' in targetDataset) onTabRight()
+})
+document.addEventListener('change', e => {
+    const targetDataset = e.target.dataset;
 
-        blocksBlock.style.display = 'flex';
-        pagesBlock.style.display = 'none';
-
-        tabLeft.classList.add('active');
-        tabRight.classList.remove('active');
-
-        if (changesCheck.checked) {
-            changesCheck.checked = false;
-        };
-        if (cmsCheck.checked) {
-            cmsCheck.checked = false;
-        };
-        total = land;
-        totalValue.value = total;
-    }
+    if ('blocksInput' in targetDataset) countBlocks()
+    if ('pagesInput' in targetDataset) countPages()
+    if ('hoursInput' in targetDataset) countHoursCost()
+    if ('rateInput' in targetDataset) countHoursCost()
+    if ('cmsCheck' in targetDataset) checkCms()
+    if ('editCheck' in targetDataset) checkEdit()
 })
 
-tabRight.addEventListener('click', () => {
+function onTabLeft() {
+    clearInputs()
+    blocksCounter.style.display = 'flex';
+    pagesCounter.style.display = 'none';
+    tabLeft.classList.add('active');
+    tabRight.classList.remove('active');
+    total = landBasePrice;
+    blocksCount = landBlocksCount;
+
+    blocksInput.value = blocksCount;
+    totalValue.value = total;
+}
+
+function onTabRight() {
+    clearInputs();
+    blocksCounter.style.display = 'none';
+    pagesCounter.style.display = 'flex';
+    tabRight.classList.add('active');
+    tabLeft.classList.remove('active');
+    total = multipageBasePrice;
+    pagesCount = pagesBaseCount;
+
+    pagesInput.value = pagesCount;
+    totalValue.value = total;
+}
+function checkCms() {
+    cmsCheck.checked
+        ? totalChecked += cmsPrice
+        : totalChecked -= cmsPrice;
+    totalValue.value = total + totalChecked;
+}
+function checkEdit() {
+    editCheck.checked
+        ? totalChecked += editTextPrice
+        : totalChecked -= editTextPrice;
+    totalValue.value = total + totalChecked;
+}
+function countBlocks() {
+    clearRate();
+    blocksCount = getBlocksCount();
+    total = blocksCount * blockPrice;
+    totalValue.value = total + totalChecked;
+}
+function countPages() {
+    clearRate();
+    pagesCount = getPagesCount();
+    total = pagesCount * pagePrice;
+    totalValue.value = total + totalChecked;
+}
+
+function countHoursCost() {
+    clearFields()
+    hours = getHours();
+    hourRate = getHourRate();
+    total = hours * hourRate;
+    totalValue.value = total + totalChecked;
+}
+
+function getBlocksCount() {
+    return blocksInput.value !== ''
+        ? parseInt(blocksInput.value)
+        : landBlocksCount;
+}
+function getPagesCount() {
+    return pagesInput.value !== ''
+        ? parseInt(pagesInput.value)
+        : pagesBaseCount;
+}
+function getHours() {
+    return hoursInput.value !== ''
+        ? parseInt(hoursInput.value)
+        : defaultHours;
+}
+function getHourRate() {
+    return rateInput.value !== ''
+        ? parseInt(rateInput.value)
+        : defaultHourRate;
+}
+
+function clearInputs() {
     for (let i = 0; i < input.length; i++) {
         input[i].value = '';
     }
-    blocksBlock.style.display = 'none';
-    pagesBlock.style.display = 'flex';
-
-    tabRight.classList.add('active');
-    tabLeft.classList.remove('active');
-
-    if (changesCheck.checked) {
-        changesCheck.checked = false;
-    };
-    if (cmsCheck.checked) {
-        cmsCheck.checked = false;
-    };
-    total = corp;
-    totalValue.value = total;
-});
-counterBlock.addEventListener('change', () => {
-    counterHours.value = '';
-    counterRate.value = '';
-    total = counterBlock.value * blocks;
-    totalValue.value = total;
-
-});
-counterPages.addEventListener('change', () => {
-    counterHours.value = '';
-    counterRate.value = '';
-    total = counterPages.value * pages;
-    totalValue.value = total;
-
-});
-counterHours.addEventListener('change', () => {
-    counterBlock.value = '';
-    counterPages.value = '';
+    editCheck.checked = false;
+    cmsCheck.checked = false;
+}
+function clearFields() {
+    blocksInput.value = '';
+    pagesInput.value = '';
     total = 0;
-    time = counterHours.value;
-    hourRate = time * counterRate.value;
-    totalValue.value = hourRate;
-    total = hourRate;
-
-});
-counterRate.addEventListener('change', () => {
-    counterBlock.value = '';
-    counterPages.value = '';
+}
+function clearRate() {
+    hoursInput.value = '';
+    rateInput.value = '';
     total = 0;
-    hourRate = time * counterRate.value;
-    totalValue.value = hourRate;
-    total = hourRate;
-
-});
-changesCheck.addEventListener('change', () => {
-    if (changesCheck.checked) {
-        total += changes;
-        totalValue.value = total;
-    } else {
-        total -= changes;
-        totalValue.value = total;
-    }
-});
-cmsCheck.addEventListener('change', () => {
-    if (cmsCheck.checked) {
-        total += cms;
-        totalValue.value = total;
-    } else {
-        total -= cms;
-        totalValue.value = total;
-    }
-});
+}
